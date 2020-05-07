7970678d708206b59298b8731e8ea965a4a32741
Gregory Thomas <SilentGrig@gmail.com>
2020-05-07 13:25:54 +0100

    chapter 7 exercise 4

--- chp7-arrays/evens.js
+++ chp7-arrays/evens.js
@@ -1,13 +1,9 @@
 let myArray = [
-  [1, 3, 6, 11],
-  [4, 2, 4],
-  [9, 17, 16, 0],
+  1, 3, 6, 11,
+  4, 2, 4, 9,
+  17, 16, 0,
 ];
 
-myArray.forEach(innerArray => {
-  innerArray.forEach(function (number) {
-    if (number % 2 == 0) {
-      console.log(number);
-    }
-  });
-});
+oddsAndEvensArray = myArray.map(value => (value % 2 == 0 ? 'even' : 'odd'));
+
+console.log(oddsAndEvensArray);
